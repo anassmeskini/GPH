766243da8ccecdef3642d8296e5b8c0f146b5198
Anass Meskini <anass.meskini94@gmail.com>
2019-03-21 23:08:48 +0100

    Use glpk when available

--- src/core/AvaiLPSolver.h
+++ src/core/AvaiLPSolver.h
@@ -2,12 +2,15 @@
 #define _AVAILABLE_SOLVER_HPP
 
 #include "interface/CPXSolver.h"
+#include "interface/GLPKSolver.h"
 #include "interface/SPXSolver.h"
 
 #ifdef CONCERT_CPLEX_FOUND
 using AvaiLPSolver = CPXSolver;
 #elif SOPLEX_FOUND
 using AvaiLPSolver = SPXSolver;
+#elif GLPK_FOUND
+using AvaiLPSolver = GLPKSolver;
 #endif
 
 #endif
